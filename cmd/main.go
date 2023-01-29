04f7d6fec09957e6a97f8e2e8c31717f5a4e88a6
Daniil Lyudvig <thricht3r@yandex.ru>
2023-01-29 15:48:33 +0300

    Added server shutdown processing

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,14 @@ import (
 	"L0task/pkg/repository"
 	"L0task/pkg/repository/postgres"
 	"L0task/pkg/service"
+	"context"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -55,6 +58,20 @@ func main() {
 	}
 
 	logrus.Print("Server started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("Server shutting down...")
+
+	if srvCloseErr := srv.Shutdown(context.Background()); srvCloseErr != nil {
+		logrus.Errorf("Error occured on server shutting down: %s", srvCloseErr.Error())
+	}
+
+	if dbCloseErr := db.Close(); dbCloseErr != nil {
+		logrus.Errorf("Error occured on database connection close: %s", dbCloseErr.Error())
+	}
 }
 
 func initConfig() error {
