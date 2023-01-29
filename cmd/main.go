9196047c1c3566f48bf1d31a6229ab720eabd742
Daniil Lyudvig <thricht3r@yandex.ru>
2023-01-29 15:42:31 +0300

    Added data recovery

--- cmd/main.go
+++ cmd/main.go
@@ -17,17 +17,17 @@ func main() {
 	logrus.Print("L0task app starting...")
 
 	logrus.Print("Initializing configs...")
-	if err := initConfig(); err != nil {
-		logrus.Fatalf("Error initializing configs: %s", err.Error())
+	if configErr := initConfig(); configErr != nil {
+		logrus.Fatalf("Error initializing configs: %s", configErr.Error())
 	}
 
 	logrus.Print("Loading env variables...")
-	if err := godotenv.Load(); err != nil {
-		logrus.Fatalf("Error loading env variables: %s", err.Error())
+	if envErr := godotenv.Load(); envErr != nil {
+		logrus.Fatalf("Error loading env variables: %s", envErr.Error())
 	}
 
 	logrus.Print("Initializing database...")
-	db, err := postgres.NewPostgresDB(postgres.Config{
+	db, dbErr := postgres.NewPostgresDB(postgres.Config{
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
 		Username: viper.GetString("db.username"),
@@ -35,14 +35,19 @@ func main() {
 		SSLMode:  viper.GetString("db.ssl_mode"),
 		Password: os.Getenv("DB_PASSWORD"),
 	})
-	if err != nil {
-		logrus.Fatalf("Failed to initialize db: %s", err.Error())
+	if dbErr != nil {
+		logrus.Fatalf("Failed to initialize db: %s", dbErr.Error())
 	}
 
 	logrus.Print("Starting server...")
 	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
+
+	logrus.Print("Restoring data from database to cache...")
+	if dbToCacheErr := services.SetOrdersFromDbToCache(); dbToCacheErr != nil {
+		logrus.Fatalf("Failed to restore data from database to cache: %s", dbErr.Error())
+	}
 
 	srv := new(L0task.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
